107217ba8d5e6018f9d2758a698c1cac280ab6d9
Jaloliddin-Sultonov <94798415+Jaloliddin-Sultonov@users.noreply.github.com>
2023-03-09 21:11:33 +0100

    Caesar Cipher

---
+++ Day-8 Caesar Cipher/main.py
@@ -0,0 +1,44 @@
+from art import logo
+
+alphabet = [
+    'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o',
+    'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'a', 'b', 'c', 'd',
+    'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's',
+    't', 'u', 'v', 'w', 'x', 'y', 'z'
+]
+
+
+def caesar(start_text, shift_amount, cipher_direction):
+    end_text = ""
+    if cipher_direction == "decode":
+        shift_amount *= -1
+    for char in start_text:
+        if char not in alphabet:
+            end_text += char
+        else:
+            position = alphabet.index(char)
+            new_position = position + shift_amount
+            end_text += alphabet[new_position]
+
+    print(f"Here's the {cipher_direction}d result: {end_text}")
+
+
+print(logo)  #Printing logo from art.py file
+
+while True:
+    direction = input("Type 'encode' to encrypt, type 'decode' to decrypt:\n")
+    text = input("Type your message:\n").lower()
+    shift = int(input("Type the shift number:\n"))
+
+    if shift > 26:
+        shift %= 26
+    caesar(start_text=text, shift_amount=shift, cipher_direction=direction)
+    answer = input(
+        "\nType 'yes' if you want to go again. Otherwise type 'no'\n")
+    if answer == 'yes':
+        continue  #while loop keeps working
+    elif answer == 'no':
+        print("Goodbye!!!")
+        break  #while loop stops
+    else:
+        print("Wrong input Kurwa")
