c53c374242be56888cb1d04231db47a25783134b
jaloliddinsultonov <sultonovjaloliddin07@gmail.com>
2023-03-10 01:02:07 +0100

    day 9 codes

---
+++ Day-9 codes/Exercise 2 - Dictionary in List.py
@@ -0,0 +1,24 @@
+travel_log = [
+{
+  "country": "France",
+  "visits": 12,
+  "cities": ["Paris", "Lille", "Dijon"]
+},
+{
+  "country": "Germany",
+  "visits": 5,
+  "cities": ["Berlin", "Hamburg", "Stuttgart"]
+},
+]
+#🚨 Do NOT change the code above
+
+#TODO: Write the function that will allow new countries
+#to be added to the travel_log. 👇
+def add_new_country(visited_country, number_of_visits, visited_cities):
+    travel_log.insert(2, {'country' : visited_country, 'visits' : number_of_visits, 'cities' : visited_cities})
+    
+
+
+#🚨 Do not change the code below
+add_new_country("Russia", 2, ["Moscow", "Saint Petersburg"])
+print(travel_log)
