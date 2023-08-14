72e0b3bee8e7c012c9eea8014e7050175009ba6e
jaloliddinsultonov <sultonovjaloliddin07@gmail.com>
2023-08-14 14:31:28 +0200

    exercise

---
+++ 48-Day exercises(Selenium)/selenium ex1.py
@@ -0,0 +1,18 @@
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+
+
+chrome_driver_path = r"C:\Development\chromedriver.exe"
+driver = webdriver.Chrome()
+
+driver.get("https://www.python.org/")
+
+event_times = driver.find_elements(By.CSS_SELECTOR, ".event-widget time")
+event_names = driver.find_elements(By.CSS_SELECTOR, ".event-widget li a")
+events = {}
+for n in range(len(event_times)):
+    events[n] = {
+        "time": event_times[n].text,
+        "name": event_names[n].text
+    }
+print(events)
